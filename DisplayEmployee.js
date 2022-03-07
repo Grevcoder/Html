f005eab63848ec855e482d52ad3479bdd098af71
Autoseleproject <ev.greeshma@gmail.com>
2022-03-07 15:36:23 -0600

    Updated assignment3

--- DisplayEmployee.js
+++ DisplayEmployee.js
@@ -1,23 +1,13 @@
-function getUrlValuesAndDisplayMessage() {
-    const params = new URLSearchParams(window.location.search);
-    var message;
-    var name;
-    var salary;
-    var designation;
+
+
+function loadSessionValuesAndDisplay(){
+    var employeeDetails =  sessionStorage.getItem("employeeData");
     
-    for (const param of params) {
-        if (param[0] === 'employeeName') {
-            name = param[1];
-        }
 
-        if (param[0] === 'Designation') {
-            designation = param[1];
-        }
-
-        if (param[0] === 'Salary') {
-            salary = param[1];
-        }
+    if(typeof(employeeDetails) !==  'undefined'){
+        var result = JSON.parse(employeeDetails); 
+        message = result.employeeName + ' who is a ' + result.Designamtion + ' will get ' + result.salary;
+        document.getElementById("message").innerHTML = message;
     }
-    message = name + ' who is a ' + designation + ' will get ' + salary
-    document.getElementById("message").innerHTML = message;
+    
 }
--- empdetails.js
+++ empdetails.js
@@ -6,26 +6,39 @@
         employeeDetails.joiningDate = document.getElementById('joiningDate').value;
         employeeDetails.hoursWorked = document.getElementById('hoursWorked').value;
         employeeDetails.Designamtion = document.getElementById('Designamtion').value;
+       
+        
         if (employeeDetails.employeeName == "" || employeeDetails.employeeId == "" || employeeDetails.hoursWorked == "" || employeeDetails.Designamtion == "") {
             alert("All fields are mandatory");
             return false;
         }
         var salary;
 
-
-        if (employeeDetails.Designamtion == "Manager") {
-            salary = employeeDetails.hoursWorked * 90;
-            window.location.href = "DisplayEmployee.html?employeeName=" + employeeDetails.employeeName + "&Designation=" + employeeDetails.Designamtion + "&Salary=" + salary;
+        if (employeeDetails.Designamtion==='Manager') {
+            salary =employeeDetails.hoursWorked*90;
+            employeeDetails.salary=salary;
+            
+     
         }
-        else if (employeeDetails.Designamtion == "Consultant") {
+        else if (employeeDetails.Designamtion === 'Consultant') {
             salary = employeeDetails.hoursWorked * 70;
-            var designation = window.location.replace("DisplayEmployee.html?employeeName=" + employeeDetails.employeeName + "&Designation=" + employeeDetails.Designamtion + "&Salary=" + salary);
-        }
+            employeeDetails.salary=salary;
+             
+              }
         else if (employeeDetails.Designamtion == "Trainee") {
             salary = employeeDetails.hoursWorked * 45;
-            var designation = window.location.replace("DisplayEmployee.html?employeeName=" + employeeDetails.employeeName + "&Designation=" + employeeDetails.Designamtion + "&Salary=" + salary);
+            employeeDetails.salary=salary;
+           
+        }  
+    
+
+        sessionStorage.setItem("employeeData", JSON.stringify(employeeDetails));
+            window.location.replace("DisplayEmployee.html");
+     
+    //var designation = window.location.replace("DisplayEmployee.html?employeeName=" + employeeDetails.employeeName + "&Designation=" + employeeDetails.Designamtion + "&Salary=" + salary);
+  
         }
-    }
+    
 
 
 
